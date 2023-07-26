dddd379080337234f3306cf54155a78a42233349
VeriMontenegro <veri.notas@gmail.com>
2023-07-26 08:31:56 -0300

    Melhorias do Projeto

--- Models/BibliotecaContext.cs
+++ Models/BibliotecaContext.cs
@@ -5,12 +5,16 @@ namespace Biblioteca.Models
 {
     public class BibliotecaContext : DbContext
     {
+        
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {                   
-            optionsBuilder.UseMySql("Server=localhost;DataBase=Biblioteca;Uid=root;");
+            optionsBuilder.UseMySql("Server=localhost; User ID=root; Password=; Database=Biblioteca");
         }
 
         public DbSet<Livro> Livros {get; set;}
+
         public DbSet<Emprestimo> Emprestimos {get; set;}
+        
+        public DbSet<Usuario> usuarios {get; set;}
     }
 }
